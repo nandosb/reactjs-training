2cd129590a309bf431bf45e6661118686108534e
Fernando Serrano <fernandos@eventbrite.com>
2020-07-09 17:56:26 -0300

    boilerplate for new Reactjs projects

---
+++ boilerplate-project/README.md
@@ -0,0 +1,4 @@
+# Installation
+
+- Open your terminal and run docker-compose up --force-recreate --build -d
+- Open localhost:8080 in your browser
---
+++ boilerplate-project/src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDom.render(<p>My React App</p>, document.getElementById('app'));
